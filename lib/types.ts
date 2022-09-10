184daf2ead2311252876326b818ae7a09d6a4bed
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-10 11:11:55 +0500

    feat: add DB table types

---
+++ lib/types.ts
@@ -0,0 +1,36 @@
+export interface IUser {
+  id: string | number
+  username: string
+  email: string
+  groups?: IGroup[]
+  posts?: IPost[]
+  comments?: IComment[]
+}
+
+export interface IPost {
+  id: string | number
+  title: string
+  body: string
+  createAt: Date
+  updateAt: Date
+  author: IUser
+  group: IGroup
+}
+
+export interface IGroup {
+  id: string | number
+  name: string
+  description: string
+  admins?: IUser[]
+  users?: IUser[]
+  posts?: IPost[]
+}
+
+export interface IComment {
+  id: string | number
+  body: string
+  createdAt: Date
+  updatedAt: Date
+  author: IUser
+  post: IPost
+}
