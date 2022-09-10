4016c4169e5f360803ce1b62d316e2d0a0782269
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-10 19:25:24 +0500

    update: use localStorage.getItems inside a useEffect hook

--- context/index.tsx
+++ context/index.tsx
@@ -1,26 +1,13 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useEffect, useState } from "react"
 
 interface IState {
   token: string
   username: string
 }
 
-function getDefaultState(): IState {
-  if (typeof window !== "undefined") {
-    const localState: IState = {
-      token: localStorage.getItem("jwt_token") as string,
-      username: localStorage.getItem("username") as string,
-    }
-    return localState
-  }
-  return {
-    token: "",
-    username: "",
-  }
-}
-
 const defaultState: IState = {
-  ...getDefaultState(),
+  token: "",
+  username: "",
 }
 
 interface IRootContext {
@@ -37,6 +24,14 @@ export const RootContext = createContext<IRootContext>(defaultRootContext)
 
 const RootContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, setState] = useState<IState>(defaultState)
+
+  useEffect(() => {
+    const localState: IState = {
+      token: localStorage.getItem("jwt_token") as string,
+      username: localStorage.getItem("username") as string,
+    }
+    setState(localState)
+  }, [])
 
   return (
     <RootContext.Provider value={{ state, setState }}>
