24b3b7b6bc8328977c21eb90683b213234eb2280
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-10 20:26:23 +0500

    style: update post styling

--- components/post/post.tsx
+++ components/post/post.tsx
@@ -2,55 +2,42 @@ import React, { useState } from "react"
 import Link from "next/link"
 
 import { IPost } from "lib/types"
-import { AddComment } from "components/comment"
 
-const Post = ({ id = 1 }: IPost) => {
-  const [addComment, setAddComment] = useState(false)
-
-  function handleAddComment(e: React.FormEvent<HTMLButtonElement>) {
-    e.preventDefault()
-    setAddComment(!addComment)
-  }
-
+const Post = ({ id = 1, group, author, body, createAt, title }: IPost) => {
   return (
-    <Link href={`/comments/${id}`}>
-      <a className="min-w-full bg-white py-4 px-6 rounded-md border border-slate-200 hover:bg-slate-50">
-        <h3 className="font-semibold">GroupName</h3>
+    <div className="min-w-full bg-white py-4 px-6 rounded-md border border-slate-200">
+      <h3 className="text-sm text-slate-600 flex items-center gap-2 mb-1">
+        <span className="text-black font-semibold">GroupName</span>
+        <span>•</span>
+        <span>Username</span>
+        <span>•</span>
+        <span>
+          {new Date().toLocaleString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })}
+        </span>
+      </h3>
 
-        <h4 className="flex items-center gap-2 text-sm text-slate-600 mb-2">
-          <span>Username</span>
-          <span>
-            {new Date().toLocaleString("en-US", {
-              month: "short",
-              day: "numeric",
-              year: "numeric",
-            })}
-          </span>
-        </h4>
+      <h3 className="text-lg font-semibold leading-5 mb-1">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut,
+        voluptates?
+      </h3>
 
-        <p className="mb-2">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis animi
-          aliquid amet. Quos sed modi libero autem vero quas porro numquam
-          officiis ducimus vitae distinctio totam, dolor laudantium dolorum
-          laborum!
-        </p>
+      <p className="mb-2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis animi
+        aliquid amet. Quos sed modi libero autem vero quas porro numquam
+        officiis ducimus vitae distinctio totam, dolor laudantium dolorum
+        laborum!
+      </p>
 
-        {addComment ? (
-          <AddComment
-            postId={id}
-            addComment={addComment}
-            setAddComment={setAddComment}
-          />
-        ) : (
-          <button
-            className="px-3 py-1 border bg-white border-slate-200 rounded-md w-fit text-sm hover:bg-slate-50"
-            onClick={handleAddComment}
-          >
-            Comment
-          </button>
-        )}
-      </a>
-    </Link>
+      <Link href={`/comments/${id}`}>
+        <a className="px-3 py-1 border bg-white border-slate-200 rounded-md w-fit text-sm hover:bg-slate-50">
+          Comments
+        </a>
+      </Link>
+    </div>
   )
 }
 
