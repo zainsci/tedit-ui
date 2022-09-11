eee2a5fa1be2c6303c46a7f7075dbdf4fa254e9a
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-11 12:11:44 +0500

    update: post component

--- components/post/post.tsx
+++ components/post/post.tsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react"
+import React from "react"
 import Link from "next/link"
 
 import { IPost } from "lib/types"
 
-const Post = ({ id = 1, group, author, body, createAt, title }: IPost) => {
+const Post = ({ id, title, body, group, author, createdAt }: IPost) => {
   return (
-    <div className="min-w-full bg-white py-4 px-6 rounded-md border border-slate-200">
+    <div className="min-w-full bg-white py-4 px-6 rounded-md border border-slate-200 mb-4">
       <h3 className="text-sm text-slate-600 flex items-center gap-2 mb-1">
-        <span className="text-black font-semibold">GroupName</span>
+        <Link href={`/group/${group?.name}`}>
+          <a className="text-black font-semibold hover:underline">
+            {group?.name}
+          </a>
+        </Link>
         <span>•</span>
-        <span>Username</span>
+        <Link href={`/user/${author?.username}`}>
+          <a className="hover:underline">{author?.username}</a>
+        </Link>
         <span>•</span>
         <span>
-          {new Date().toLocaleString("en-US", {
+          {new Date(createdAt).toLocaleString("en-US", {
             month: "short",
             day: "numeric",
             year: "numeric",
@@ -20,17 +26,9 @@ const Post = ({ id = 1, group, author, body, createAt, title }: IPost) => {
         </span>
       </h3>
 
-      <h3 className="text-lg font-semibold leading-5 mb-1">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut,
-        voluptates?
-      </h3>
+      <h3 className="text-lg font-semibold leading-5 mb-1">{title}</h3>
 
-      <p className="mb-2">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis animi
-        aliquid amet. Quos sed modi libero autem vero quas porro numquam
-        officiis ducimus vitae distinctio totam, dolor laudantium dolorum
-        laborum!
-      </p>
+      <p className="mb-2 leading-5">{body}</p>
 
       <Link href={`/comments/${id}`}>
         <a className="px-3 py-1 border bg-white border-slate-200 rounded-md w-fit text-sm hover:bg-slate-50">
