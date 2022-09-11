cc5eabe4d94bdc964977ff23d97beb9b8ca51669
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-11 18:44:01 +0500

    feat: Loader

---
+++ components/loader/loader.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+const Loader = () => {
+  return (
+    <div className="w-full flex justify-center items-center animate-spin">
+      <svg
+        height="100%"
+        viewBox="0 0 32 32"
+        width="100%"
+        className="w-6 h-6 text-purple-500"
+      >
+        <circle
+          cx="16"
+          cy="16"
+          fill="none"
+          r="14"
+          strokeWidth="4"
+          style={{ stroke: "currentColor", opacity: "0.2" }}
+        ></circle>
+        <circle
+          cx="16"
+          cy="16"
+          fill="none"
+          r="14"
+          strokeWidth="4"
+          style={{
+            stroke: "currentColor",
+            strokeDasharray: 80,
+            strokeDashoffset: 60,
+          }}
+        ></circle>
+      </svg>
+    </div>
+  )
+}
+
+export default Loader
